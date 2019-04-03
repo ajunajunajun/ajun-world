f240f98019bd1519f4f61c31651707688d9bfa2a
ajunajunajun <ajunnajunn@gmail.com>
2019-04-04 00:47:41 +0900

    Tobe usabled css and create TopPage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 
-import Select from './components/Select/index';
-import Top from './components/Top/index';
-import Google from './components/Google/index';
+import Select from './components/Select/select';
+import Top from './components/Top/top';
+import Google from './components/Google/google';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <Select />
-        </div>
+        <Select />
         <body>
           <Switch>
             <Route exact path='/' component={ Top } />
