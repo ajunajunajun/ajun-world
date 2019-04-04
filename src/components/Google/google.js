4268070568c26d0b82c89465cd6c8788b9e35811
ajunajunajun <ajunnajunn@gmail.com>
2019-04-05 02:38:26 +0900

    create GoogleSearchForm

--- src/components/Google/google.js
+++ src/components/Google/google.js
@@ -1,11 +1,123 @@
 import React from 'react';
+import styled,{ css } from 'styled-components';
 
-import styled from 'styled-components';
+const GoogleDiv = styled.div`
+  min-width: 980px;
+`;
+const GoogleHeaderDiv = styled.div`
+  text-align: right;
+  height: 50px;
+`;
+const GoogleHeaderP = styled.p`
+  display: inline;
+  margin: 0px 8px 0px 8px;
+  vertical-align: middle;
+  line-height: 60px;
+  font-size: small;
+  height: 100%;
+`;
+const GoogleHeaderImg1 = styled.img`
+  margin: 0px 10px 0px 10px;
+  vertical-align: middle;
+  height: 25px;
+`;
+const GoogleHeaderImg2 = styled.img`
+  margin: 0px 32px 0px 10px;
+  vertical-align: middle;
+  height: 25px;
+`;
+const GoogleMainDiv = styled.div`
+  height: 398px;
+`;
+const GoogleLogoDiv = styled.div`
+  height: 233px;
+  margin-top: 39px;
+`;
+const GoogleLogoImg = styled.img`
+  padding-top: 109px;
+`;
+const GoogleSearchDiv = styled.div`
+  height: 118px;
+`;
+const GoogleSearchformDiv = styled.div`
+  height: 46px;
+`;
+const GoogleFormDiv = styled.div`
+  height: 44px;
+  width: 484px;
+  margin: auto;
+  padding-left: 12px;
+`;
+const GoogleForm = styled.form`
+  border: 1px #000 solid;
+  height: 100%;
+`;
+const GoogleMarkDiv = styled.div`
+  height: 44px;
+  width: 64px;
+`;
+const GoogleMarkImg = styled.img`
+  margin: auto;
+  padding-top: 10px;
+`;
+const GoogleButtonDiv = styled.div`
+  height: 100%;
+  width: 484px;
+  margin: auto;
+  padding-top: 20px;
+`;
+const GoogleButtonDiv1 = styled.div`
+  width: 272px;
+  height: 100%;
+  margin: auto;
+  position: relative;
+`;
+const GoogleInput1 = styled.input`
+  height: 36px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
+const GoogleInput2 = styled.input`
+  height: 36px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
 
 class Google extends React.Component{
   render(){
     return(
-      <p>Google</p>
+      <GoogleDiv>
+        <GoogleHeaderDiv>
+          <GoogleHeaderP>Gmail</GoogleHeaderP>
+          <GoogleHeaderP>画像</GoogleHeaderP>
+          <GoogleHeaderImg1 src='./imgs/Google/sikaku.png' alt="sikaku" />
+          <GoogleHeaderImg2 src='./imgs/Google/hiyoko.png' alt="user" />
+        </GoogleHeaderDiv>
+        <GoogleMainDiv>
+          <GoogleLogoDiv>
+            <GoogleLogoImg src='./imgs/Google/googlelogo_color_272x92dp.png' alt="googlelogo" />
+          </GoogleLogoDiv>
+          <GoogleSearchDiv>
+            <GoogleSearchformDiv>
+              <GoogleFormDiv>
+                <GoogleForm>
+                  <GoogleMarkDiv>
+                   <GoogleMarkImg src='./imgs/Google/mark.png' alt="searghmark" />
+                  </GoogleMarkDiv>
+                </GoogleForm>
+                <GoogleButtonDiv>
+                  <GoogleButtonDiv1>
+                    <GoogleInput1 value="　Google 検索　" type="submit"/>
+                    <GoogleInput2 value="　I'm Feeling Lucky　" type="submit"/>
+                  </GoogleButtonDiv1>
+                </GoogleButtonDiv>
+              </GoogleFormDiv>
+            </GoogleSearchformDiv>
+          </GoogleSearchDiv>
+        </GoogleMainDiv>
+      </GoogleDiv>
     )
   }
 }
--- src/components/Top/top.js
+++ src/components/Top/top.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import styled,{ css } from 'styled-components';
+import styled from 'styled-components';
 
 const TopDiv = styled.div`
   border: 1px #884898 solid;
 `;
 const TopH1Div = styled.div`
 `;
-const TopImgDiv = styled.div`
+const TopMainDiv = styled.div`
 `;
 const TopImg = styled.img`
   height: 200px;
@@ -21,14 +21,14 @@ class Top extends React.Component{
         <TopH1Div>
           <h1>THIS IS AJUN WORLD</h1>
         </TopH1Div>
-        <TopImgDiv>
+        <TopMainDiv>
           <TopImg src='./imgs/Top/co2411036.jpg' alt="topimg0" />
           <TopImg src='./imgs/Top/6Uxe0IWs.png' alt="topimg1" />
           <TopImg src='./imgs/Top/ひよこ.jpg' alt="topimg2" />
           <TopImg src='./imgs/Top/s98tzpZW.jpg' alt="topimg3" />
           <TopImg src='./imgs/Top/hiyoko.png' alt="topimg4" />
           <TopImg src='./imgs/Top/S__7381006.jpg' alt="topimg5" />
-        </TopImgDiv>
+        </TopMainDiv>
       </TopDiv>
     )
   }
