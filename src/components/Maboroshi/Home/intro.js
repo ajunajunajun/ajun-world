5d31ca03501e335f2eb88772ce8a7f173570da60
ajun <ajunnajunn@gmail.com>
2019-05-24 11:22:54 +0900

    create base of intro

---
+++ src/components/Maboroshi/Home/intro.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const IntroDiv = styled.div`
+  padding-top: calc(50vh - 63px);
+  background-color: #dcdcdc;
+  overflow: hidden;
+`;
+const TitleDiv = styled.div`
+  margin: 0;
+  padding: 0;
+`;
+const TitleImgDiv = styled.div`
+  margin: auto;
+  width: 336px;
+  height: 116px;
+`;
+const TitleImg = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #a9a9a9;
+`;
+const DataCarouselDiv = styled.div`
+  margin-top: 144px;
+`;
+const CarouselItemDiv = styled.div`
+  width:2400px;
+  display:table;
+`;
+const CarouselItem = styled.div`
+  width:800px;
+  height:800px;
+  background-color: #a9a9a9
+  display:table-cell;
+`;
+export default class Intro extends React.Component{
+  render(){
+    return(
+      <IntroDiv className="Intro">
+        <TitleDiv>
+          <TitleImgDiv>
+            <TitleImg/>
+          </TitleImgDiv>
+        </TitleDiv>
+        <DataCarouselDiv>
+          <CarouselItemDiv>
+            <CarouselItem/>
+            <CarouselItem/>
+            <CarouselItem/>
+          </CarouselItemDiv>
+        </DataCarouselDiv>
+      </IntroDiv>
+    );
+  }
+}
